1594f752f49767d552fef04c18bfbc7268f0ffa9
williamsza <william.sza@outlook.com>
2022-01-14 16:57:19 -0300

    Capititurar elementos manipulados

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 script.js
+
+const image = document.querySelector('img');
+const botao = document.querySelector('buton');
+const momeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('especie');
+const status = document.querySelector('status');
