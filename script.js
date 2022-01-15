549be59f8dbcb23dffd70c39d5d4ac8193458dfe
williamsza <william.sza@outlook.com>
2022-01-14 22:29:52 -0300

    Criar consumo de API RICK And Morty

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ const image = document.querySelector('img');
 const botao = document.querySelector('buton');
 const momeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('especie');
-const status = document.querySelector('status');
+const condicao = document.querySelector('status');
+
+pegarPersonagem = () => {
+
+    return fetch(`https://rickendmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "content-type": 'application/json'
+    }).then((Response)) => Response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.mame;
+        momeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.especies;
+        condicao.innerHTML = data.status;
+
+    
+    });
+    
+}
+botao.onclik = pegarImagem;
